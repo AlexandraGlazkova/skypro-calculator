6893959f3b2069adcec64575fbbbece27c879809
GlakovaA <22109@bk.ru>
2022-09-09 15:17:07 +0300

    вот теперь точно готова)))

---
+++ src/main/java/pro/sky/skyprocalculator/CalController.java
@@ -0,0 +1,44 @@
+package pro.sky.skyprocalculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalController {
+    private final CalService service;
+
+    public CalController(CalService service) {
+        this.service=service;
+    }
+    @GetMapping
+    public String calculator() {
+        return service.calculator();
+    }
+    @GetMapping(path = "/calculator")
+    public String dataCalculator() {
+        return service.dataCalculator();
+    }
+    @GetMapping (path= "/calculator/plus")
+    public String plusCalculator (@RequestParam (value = "num1", required = false) Integer num1,
+                                  @RequestParam (value = "num2", required = false) Integer num2)
+    {
+        return service.plusCalculator(num1, num2);
+    }
+    @GetMapping (path= "/calculator/minus")
+    public String minusCalculator (@RequestParam (value = "num1", required = false) Integer num1,
+                                   @RequestParam (value = "num2", required = false) Integer num2){
+
+        return service.minusCalculator(num1, num2);
+    }
+    @GetMapping (path= "/calculator/multiply")
+    public String multiplyCalculator (@RequestParam (value = "num1", required = false) Integer num1,
+                                      @RequestParam (value = "num2", required = false) Integer num2) {
+        return service.multiplyCalculator(num1, num2);
+    }
+    @GetMapping (path= "/calculator/divide")
+    public String divideCalculator (@RequestParam (value = "num1", required = false) Integer num1,
+                                    @RequestParam (value = "num2", required = false) Integer num2) {
+        return service.divideCalculator(num1, num2);
+    }
+}
---
+++ src/main/java/pro/sky/skyprocalculator/CalService.java
@@ -0,0 +1,45 @@
+package pro.sky.skyprocalculator;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Service
+public class CalService {
+    public String calculator() {
+        return "<b>Добро пожаловать в калькулятор</b>";
+    }
+    public String dataCalculator() {
+        return "Приступим!";
+    }
+    public String plusCalculator (Integer num1, Integer num2) {
+        if (num1 == null || num2 == null || num1==null && num2==null) {
+            return "Ошибка!!! Введены не все данные!!!";
+        } else {
+            return num1 + "+" + num2 + "=" + (num1 + num2);
+        }
+    }
+    public String minusCalculator (Integer num1,  Integer num2) {
+        if (num1 == null || num2 == null || num1 == null && num2 == null) {
+            return "Ошибка!!! Введены не все данные!!!";
+        } else {
+            return num1 + "-" + num2 + "=" + (num1 - num2);
+        }
+    }
+    public String multiplyCalculator (Integer num1,  Integer num2) {
+        if (num1 == null || num2 == null || num1==null && num2==null) {
+             return "Ошибка!!! Введены не все данные!!!";
+        } else {
+             return num1 + "*" + num2 + "=" + (num1*num2);
+            }
+        }
+    public String divideCalculator (Integer num1,  Integer num2) {
+            if (num1 == null || num2 == null || num1==null && num2==null) {
+                return "Ошибка!!! Введены не все данные!!!";
+            } else if (num2==0) {
+                return "ОШИБКА!!! на 0 делить нельзя!!!";
+            } else {
+                return num1 + "/" + num2 + "=" + num1/num2;
+        }
+    }
+}
